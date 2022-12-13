818262ce9c94183888377b6ba15cf0b41ac03cdb
whokilleddb <whokilleddb@gmail.com>
2022-12-13 13:36:29 +0530

    Replace derive with builder

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,78 @@
-//use url::Url;
-use ctrlc;
-use std::io;
-use clap::Parser;
-use std::io::Write;
-use std::error::Error;
-
-/// User defined modules
 mod user_struct;
+use crate::user_struct::*;
+use clap::{Arg, Command, ArgAction};
 
-/// Function to get information interactive mode
-fn interactive_mode_setup(cmd_line: &mut user_struct::CmdOptions) -> Result <(), Box<dyn Error>> {
-    // Get User input
-    let get_user_input = |msg: &str| {
-        let mut buf: String = String::new();
-
-        print!("[i] {}", msg);
-        match io::stdout().flush(){
-            Ok(_v) => _v,
-            Err(e) => {
-                eprintln!("[!] Error occured as: {}", e);
-                return None;
-            },
-        };
-
-        match io::stdin().read_line(&mut buf){
-            Ok(_v) => _v,
-            Err(e) => {
-                eprintln!("[!] Error occured as: {}", e);
-                return None;
-            },
-        };
-
-        buf = buf.trim().to_string();
-        Some(buf.clone())
-    };
-
-    // Closure to read user input and return a boolean to Y/N questions
-    let get_choice = | msg: &str | {
-        let user_input = get_user_input(msg).unwrap();
-        let choice = match user_input.as_str().chars().next(){
-            Some(_v) => {
-                let ch = _v.to_ascii_uppercase();
-                if ch == 'Y' {
-                    true
-                }
-                else {
-                    false
-                }
-            },
-            None => false
-        };
-        choice
-    };
-
-    // Get URL
-    loop {
-        let _url = match get_user_input("Enter remote PE url: ") {
-            Some(val) => val.to_string(),
-            None => return Err("Failed to read remote PE URL!".into()),
-        };
-
-        if  !_url.is_empty() {
-            cmd_line.url = _url;
-            break;
-        }
-        eprintln!("[!] URL value cannot be empty!");
-    }
-
-    // Check for encryption
-    cmd_line.enc = get_choice("Is the incoming binary encrypted?(y/N): ");
-
-    // Get Key for Encryption
-    if cmd_line.enc {
-        loop {
-            let _key = match get_user_input("Enter Decryption Key: ") {
-                Some(val) => val.to_string(),
-                None => return Err("Failed to read decryption key!".into()),
-            };
-
-            if  !_key.is_empty() {
-                cmd_line.key = Some(_key);
-                break;
-            }
-            eprintln!("[!] Key value cannot be empty!");
-        }
-    }
-
-    // Patching and Detection
-    cmd_line.patch_amsi = get_choice("Patch AMSI?(y/N): ");
-    cmd_line.patch_amsi = get_choice("Patch ETW?(y/N): ");
-    cmd_line.detect_sandbox = get_choice("Detect Sandbox?(y/N): ");
-
-    // Get printing mode
-    let printing_mode = get_user_input("Printing mode([n]ormal/[Q]uiet/[V]erbose):");
-    match printing_mode.unwrap().as_str().chars().next(){
-        Some(_v) => {
-            let ch = _v.to_ascii_uppercase();
-            if ch == 'V' {
-                cmd_line.verbose = true;
-                cmd_line.quiet = false;
-            }
-            else if ch == 'Q'{
-                cmd_line.verbose = false;
-                cmd_line.quiet = true;
-            }
-            else {
-                cmd_line.verbose = false;
-                cmd_line.quiet = false;
-            }
-        },
-        None => {
-            cmd_line.verbose = false;
-            cmd_line.quiet = false;
-        }
-    };
-
-    Ok(())
-}
-
-
-/// Ctrl-C Handler
-fn set_ctrl_c_handler(quiet: bool) {
-    // Set Ctrl-C handler
-    match ctrlc::set_handler(move || {
-        if !quiet {
-            eprintln!("\n[i] Received Ctrl+C!");
-        }
-        std::process::exit(0);
-    }){
-        Ok(_r) => _r,
-        Err(_e) => {
-            if !quiet {
-                eprintln!("\n[i] Failed to set control handler!");
-            }
-            std::process::exit(-1);
-        }
-    }
-}
-
-
-#[tokio::main]
-async fn main(){
-    let mut cmd_options = user_struct::CmdOptions::parse();
-    if cmd_options.interactive {
-        match interactive_mode_setup(&mut cmd_options){
-            Ok(_v) => _v,
-            Err(e) => {
-                eprintln!("[!] Error occured as: {:?}", e);
-                std::process::exit(-2);
-            }
-        };
-    }
-    println!("{}",cmd_options);
-    set_ctrl_c_handler(cmd_options.quiet);
-
+fn main() {
+    let matches = Command::new("Exe Who?")
+        .version("0.1.1")
+        .author("DB <whokilleddb@proton.me>")
+        .about("Run executables in Memory!")
+        .arg(
+            Arg::new("url")
+                .short('u')
+                .long("url")
+                .help("URL to fetch executable from")
+                .required(true)
+        )
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .help("Print verbose outputs")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("quiet")
+        )
+        .arg(
+            Arg::new("quiet")
+                .short('q')
+                .long("quiet")
+                .help("Supress outputs")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("verbose")
+        )
+        .arg(
+            Arg::new("patch_amsi")
+                .long("pa")
+                .help("Patch AMSI")
+                .action(ArgAction::SetTrue)
+        )
+        .arg(
+            Arg::new("patch_etw")
+                .long("pe")
+                .help("Patch ETW")
+                .action(ArgAction::SetTrue)
+        )
+        .arg(
+            Arg::new("detect_sandbox")
+                .short('d')
+                .long("ds")
+                .help("Try to detect if loader is in a Sandbox")
+                .action(ArgAction::SetTrue)
+        )
+        .arg(
+            Arg::new("dec_key")
+                .short('k')
+                .long("key")
+                .help("Key for decrypting incoming stream(if encrypted)")
+        )
+        .arg(
+            Arg::new("move")
+                .short('m')
+                .long("move")
+                .help("Move binary to another location")
+                .num_args(0..=1)
+                .require_equals(true)
+                .default_missing_value("%USERPROFILE%/AppData")
+        )
+        .arg(
+            Arg::new("use_clipboard")
+                .short('c')
+                .long("clipboard")
+                .help("[Exp] Store payload in clipboard buffer")
+                .action(ArgAction::SetTrue)
+        )
+        .get_matches();
+    
+    let mut loader_options = LoaderOptions::new();
+    loader_options.parse(matches);
 }
--- src/user_struct.rs
+++ src/user_struct.rs
@@ -1,106 +1,28 @@
-//! This file contains all structs required by the program at various stages
+#![allow(dead_code)]
+use url::Url;
 use std::fmt;
-use clap::Parser;
-use std::option::Option;
+use clap::ArgMatches;
 
-/// Struct to store command line options
-#[derive(Debug, Parser)]
-#[command(name = "Exe who?")]
-#[command(author = "whokilleddb <whokilleddb@proton.me>")]
-#[command(version = "1.0")]
-#[command(about = "Run Executables In-memory!", long_about = None)]
-pub struct CmdOptions {
-    /// Run in interactive mode
-    #[arg(short, long)]
-    pub interactive: bool,            
-
-    /// Url to fetch executable from
-    #[arg(short, long, conflicts_with = "interactive")]
-    pub url: String,            
-    
-    /// Incoming executable is encrypted
-    #[arg(short, long="encrypted", requires = "decrypt")]
-    pub enc: bool,                      
-
-    /// Key to be used for decryption
-    #[arg(short, long, group = "decrypt")]
-    pub key: Option<String>,            
-    
-    /// Patch AMSI
-    #[arg(long="pa")]
-    pub patch_amsi: bool,               
-    
-    /// Patch ETW
-    #[arg(long="pe")]
-    pub patch_etw: bool,                
-    
-    /// Detect Sandbox
-    #[arg(short, long)]
-    pub detect_sandbox: bool,           
-    
-    /// Print Minimal messages
-    #[arg(short, long, conflicts_with = "verbose")]
-    pub quiet: bool,               
-
-    /// [Experimental Feature] Save Payload in Clipboard Buffer
-    #[arg(short='c', long="clipboard")]
-    pub use_clipboard: bool,  
-
-    /// Move executable to AppData
-    #[arg(short='m', long="move")]
-    pub move_exe: bool,
-    
-    /// Location to be moved to 
-    #[arg(short, long)]
-    pub location: Option<String>,
-
-    /// Print verbose messages
-    #[arg(short, long, conflicts_with = "quiet")]
-    pub verbose: bool              
+/// Struct to store options 
+#[derive(Clone)]
+pub struct LoaderOptions {
+    url: Url,
+    verbosity: i32, 
 }
 
-
-/// Properly display values
-impl fmt::Display for CmdOptions {
+/// Dictate how the contents of the struct will be printed
+impl fmt::Display for LoaderOptions {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let printing_mode = {
-            if self.quiet {
-                "Quiet"
-            }
-
-            else if self.verbose {
-                "Verbose"
-            }
-
-            else {
-                "Normal"
-            }
-        };
-
         let mut msg = format!("[i] URL:\t\t\t{}\r\n", self.url);
-        msg = format!("{}[i] Encrypted:\t\t\t{}\r\n",msg, self.enc);
-        if self.enc {
-            let dec_key = match &self.key {
-                Some(v) => v.as_str(),
-                None => "None"
-            };
-                
-            msg = format!("{}[i] Decryption Key:\t\t{}\r\n", msg, dec_key);
-        }
-        msg = format!("{}[i] Patch AMSI:\t\t\t{}\r\n",msg, self.patch_amsi);
-        msg = format!("{}[i] Patch ETW:\t\t\t{}\r\n",msg, self.patch_etw);
-        msg = format!("{}[i] Detect Sandbox:\t\t{}\r\n",msg, self.detect_sandbox);
-        msg = format!("{}[i] Printing Mode:\t\t{}\r\n", msg, printing_mode);
-        msg = format!("{}[i] Use Clipboard Buffer:\t{}\r\n", msg, self.use_clipboard);
-        
-        if self.move_exe {
-            let move_path: &str = match &self.location {
-                Some(v) => v.as_str(),
-                None => "None"
-            };    
-            msg = format!("{}[i] Move Binary To:\t\t{}\r\n", msg, move_path);
-        }
-
-        write!(f, "{}", msg)
+        write!(f,"{}",msg)
     }
+}
+
+/// Implement various traits for LoaderOptions
+impl LoaderOptions {
+    /// Return new LoaderOptions struct
+    pub fn new(&self, cmds: ArgMatches) {
+        let url = cmds.get_one::<String>("url");
+        println!("{:?}", url);
+    } 
 }
