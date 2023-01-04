c3b53327627e1966c25cbfc407a9e88b4f3101d0
whokilleddb <whokilleddb@gmail.com>
2023-01-04 17:17:26 +0530

    Added list of sus strings

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -1,13 +1,36 @@
 use std::option::Option;
+use std::collections::HashMap;
 use sysinfo::{System, SystemExt};
 
+// https://github.com/LordNoteworthy/al-khaser/issues/189
+const IDENTIFIER_STRINGS: &'static [&str] = &["virtual", "vmware", "vboxuser", "ieuser", 
+                                                "currentuser", "sandbox", "user", "win7-traps", 
+                                                "testuser", "maltest", "malware", "fortinet", 
+                                                "flarevm", "johndoe"];
+
 pub fn is_sandbox()-> bool {
+    let sandbox;
+    sandbox = t1497_001();
+    return sandbox;
+}
+
+fn t1497_001()->bool{
+    println!("[+] Performing T1497.001 System Checks");
+
+    let mut userinfo: HashMap<&str, Option<String>> = HashMap::new();
+
+    // Check for suspicious strings
     let has_sus_str = |name: Option<String>|{
         match name {
             Some(v) => {
-                let name_lower_case = v.to_ascii_lowercase();
-                if name_lower_case.contains("virtual") || name_lower_case.contains("vmware") {
-                    return true;
+                let name_lower_case = v.to_ascii_lowercase()
+                                                .replace(&[
+                                                    '(', ')', ',','-', '\"', '.', ';', ':', '\''
+                                                    ][..], "");
+                for identifier in IDENTIFIER_STRINGS {
+                    if name_lower_case.contains(identifier) {
+                        return true;
+                    }
                 }
                 return false;
             }
@@ -17,15 +40,24 @@ pub fn is_sandbox()-> bool {
         }
     };
 
-    let mut sandbox = false;
-    println!("[+] Performing T1497.001 System Checks");
+    // Check username for common names used by sandboxes
+    // Used by: Astaroth
+    userinfo.insert("Realname", Some(whoami::realname()));
+    userinfo.insert("Devicename", Some(whoami::devicename()));
+    userinfo.insert("Hostname", Some(whoami::hostname()));
+    userinfo.insert("Username", Some(whoami::username()));
 
-    // Please note that we use "new_all" to ensure that all list of
-    // components, network interfaces, disks and users are already
-    // filled!
+    for (k, v) in userinfo.iter(){
+        if has_sus_str(v.clone()) {
+            println!("[i] {} contains a suspicious string!", k);
+            return true;
+        }
+        println!("[i] {}\t\t\t\t{}", k, v.as_ref().unwrap());
+    }
+
+
+    // Get system information
     let mut sys = System::new_all();
-
-    // First we update all information of our `System` struct.
     sys.refresh_all();
     
     // Enumerating all Network interfaces
@@ -33,22 +65,22 @@ pub fn is_sandbox()-> bool {
     for (interface_name, _data) in sys.networks() {
         if has_sus_str(Some(interface_name.clone())) {
             println!("[!] Found Interface\t\t{}", interface_name);
-            sandbox = true;
+            return true;
         }
     }
 
     // Check system name
+    println!("[i] Enumerating System Name");
     if has_sus_str(sys.name()) {
-        println!("[!] System Name\t\t{}", sys.name().unwrap());
-        sandbox = true;
+        println!("[!] Suspicious System Name\t\t{}", sys.name().unwrap());
+        return true;
     }
 
     // Check Hostname
+    println!("[i] Enumerating Host Name");
     if has_sus_str(sys.host_name()) {
-        println!("[!] System Name\t\t{}", sys.host_name().unwrap());
-        sandbox = true;
+        println!("[!] Suspicious Host Name\t\t{}", sys.host_name().unwrap());
+        return true;
     }
-
-    return sandbox;
+    return false;
 }
-
